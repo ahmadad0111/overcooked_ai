9f643b42ab0ad8e291ef44948ec0188a07b75233
VediYD <vedi.yash@gmail.com>
2025-02-08 17:55:57 +1100

    update db push logic

--- src/overcooked_demo/server/database.py
+++ src/overcooked_demo/server/database.py
@@ -13,52 +13,54 @@ class Database:
         self.cursor = self.conn.cursor()
 
     def update(self, data):
-        """Inserts a new record and its transition data into the database."""
+        """Inserts new records and their transition data into the database."""
         try:
-            transition = data["trajectory"]
+            transition_list = data["trajectory"]
+            commit_hash = data["hash_key"]
 
-            # Insert into trajectories table
+            # Prepare insert query for the trajectories table
             insert_trajectory_query = """
             INSERT INTO trajectories (
-                state, joint_action, reward, time_left, score, time_elapsed, 
+                hash_key, state, joint_action, reward, time_left, score, time_elapsed, 
                 cur_gameloop, layout, layout_name, trial_id, player_0_id, 
                 player_1_id, player_0_is_human, player_1_is_human, timestamp
             ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             RETURNING id;
             """
 
-            self.cursor.execute(
-                insert_trajectory_query,
-                (
-                    transition["state"],
-                    transition["joint_action"],
-                    transition["reward"],
-                    transition["time_left"],
-                    transition["score"],
-                    transition["time_elapsed"],
-                    transition["cur_gameloop"],
-                    transition["layout"],
-                    transition["layout_name"],
-                    transition["trial_id"],
-                    transition["player_0_id"],
-                    transition["player_1_id"],
-                    transition["player_0_is_human"],
-                    transition["player_1_is_human"],
-                    transition["timestamp"]
-                ),
-            )
-
-            trajectory_id = self.cursor.fetchone()[0]
+            # Iterate over the list of trajectory dictionaries and insert them
+            for transition in transition_list:
+                self.cursor.execute(
+                    insert_trajectory_query,
+                    (
+                        commit_hash,  # Insert hash_key first
+                        transition['state'],
+                        transition["joint_action"],
+                        transition["reward"],
+                        transition["time_left"],
+                        transition["score"],
+                        transition["time_elapsed"],
+                        transition["cur_gameloop"],
+                        transition["layout"],
+                        transition["layout_name"],
+                        transition["trial_id"],
+                        transition["player_0_id"],
+                        transition["player_1_id"],
+                        transition["player_0_is_human"],
+                        transition["player_1_is_human"],
+                        transition["timestamp"]
+                    ),
+                )
 
             # Insert into records table
             insert_record_query = """
-            INSERT INTO records (uid, timestamp, trajectory_id) VALUES (%s, %s, %s);
+            INSERT INTO records (uid, timestamp, hash_key) VALUES (%s, %s, %s);
             """
-            self.cursor.execute(insert_record_query, (data["uid"], data['timestamp'], trajectory_id))
+
+            self.cursor.execute(insert_record_query, (data["uid"], data['timestamp'], commit_hash))
 
             # Commit the transaction
             self.conn.commit()
-            print(f"Inserted record with UID {data['uid']} and Trajectory ID {trajectory_id} at {data['timestamp']}")
 
         except psycopg2.Error as e:
             self.conn.rollback()
