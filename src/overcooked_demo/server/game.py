e42a38aa27d0230ace9fc86859ddf320de1b8619
ahmadad0111 <98621372+ahmadad0111@users.noreply.github.com>
2025-02-12 19:34:15 +1100

    Update game.py

--- src/overcooked_demo/server/game.py
+++ src/overcooked_demo/server/game.py
@@ -709,13 +709,13 @@ class OvercookedGame(Game):
             except Exception as e:
                 raise IOError("Error loading agent\n{}".format(e.__repr__()))
 
-    def get_data(self,subject_id):
+    def get_data(self):
         """
         Returns and then clears the accumulated trajectory
         """
         commit_hash = str(generate_unique_hash())
         data = {
-            "uid": subject_id, #str(time()),
+            "uid": str(time()),
             "trajectory": self.trajectory,
             "timestamp": datetime.now().isoformat(),
             "hash_key": commit_hash,
