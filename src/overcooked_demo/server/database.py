4e59c784c949e1a23f581355b9ea2f243aa1f515
ahmadad0111 <98621372+ahmadad0111@users.noreply.github.com>
2025-03-13 00:33:32 +1100

    Update database.py

--- src/overcooked_demo/server/database.py
+++ src/overcooked_demo/server/database.py
@@ -2,7 +2,7 @@ import json
 import psycopg2
 
 
-class Database1:
+class Database:
     def __init__(self, config_file="db.json"):
         # Load database credentials
         with open(config_file, "r") as file:
