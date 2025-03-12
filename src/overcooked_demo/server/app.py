75cf227f612830b9141b39512c7c193a9013d86d
ahmadad0111 <98621372+ahmadad0111@users.noreply.github.com>
2025-03-13 01:25:33 +1100

    Update app.py

--- src/overcooked_demo/server/app.py
+++ src/overcooked_demo/server/app.py
@@ -371,8 +371,6 @@ def index():
 @app.route("/predefined")
 def predefined():
     uid = request.args.get("UID")
-    if uid:
-        session['UID'] = uid  # Store UID in session
     num_layouts = len(CONFIG["predefined"]["experimentParams"]["layouts"])
 
     return render_template(
